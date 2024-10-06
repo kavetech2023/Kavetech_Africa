1b3ed94ef7348701ca9d713a9155ad2c3cb85497
kavetech2023 <kavetech@gmail.com>
2024-10-06 18:19:18 +0300

    update features section

--- kavetech/src/Components/FeaturesSection.jsx
+++ kavetech/src/Components/FeaturesSection.jsx
@@ -149,13 +149,13 @@ const FeaturesSection = () => {
                   </rect>
                 </g>
               </svg>
-              <div className="absolute -bottom-6 -right-6 bg-white rounded-full p-4 shadow-xl">
+              <div className="absolute -bottom-4 -right-4 bg-white rounded-full p-3 shadow-xl">
                 <motion.div
                   initial={{ rotate: 0 }}
                   animate={{ rotate: 360 }}
                   transition={{ duration: 20, repeat: Infinity, ease: "linear" }}
                 >
-                  <svg className="w-16 h-16 text-blue-500" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <svg className="w-12 h-12 text-blue-500" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path d="M12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22Z" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
                     <path d="M12 6V12L16 14" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
                   </svg>
