95c541feb8cc2d7a93d097cec52c08c275c42ae5
kavetech2023 <kavetech@gmail.com>
2024-10-07 09:19:47 +0300

    update gtm

--- kavetech/src/App.jsx
+++ kavetech/src/App.jsx
@@ -1,15 +1,17 @@
 import { Routes, Route } from "react-router-dom";
+import GTM from 'react-gtm-module';
 import Home from "./Pages/Home";
 import AIChatBot from "./Components/AIChatBot";
 import PrivacyPolicy from "./Pages/PrivacyPolicy";
 import Portfolio from "./Pages/Portfolio";
 import Contact from "./Pages/Contact";
 import WebBuilder from "./Pages/WebBuilder";
+const GTM_ID = 'G-292S0LQ8ZR';
 const App = () => {
 
   return (
     <>
-      
+      <GTM.Initializer gtmId={GTM_ID} />
 
 
       <div className="relative  w-full bg-gradient-to-br from-indigo-100 via-purple-50 to-pink-50">
@@ -24,6 +26,7 @@ const App = () => {
           <AIChatBot />
         
       </div>
+      
     </>
   );
 };
