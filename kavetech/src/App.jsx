bbd27be4adc5f3c1dea8a683f075689090e4c9cb
kavetech2023 <kavetech@gmail.com>
2024-10-08 08:49:44 +0300

    remove web buildertool

--- kavetech/src/App.jsx
+++ kavetech/src/App.jsx
@@ -5,13 +5,13 @@ import AIChatBot from "./Components/AIChatBot";
 import PrivacyPolicy from "./Pages/PrivacyPolicy";
 import Portfolio from "./Pages/Portfolio";
 import Contact from "./Pages/Contact";
-import WebBuilder from "./Pages/WebBuilder";
-const GTM_ID = 'G-292S0LQ8ZR';
+
+
 const App = () => {
 
   return (
     <>
-      <GTM.Initializer gtmId={GTM_ID} />
+      
 
 
       <div className="relative  w-full bg-gradient-to-br from-indigo-100 via-purple-50 to-pink-50">
@@ -21,7 +21,7 @@ const App = () => {
             <Route path="/portfolio" element={<Portfolio />} />
             <Route path="/contact" element={<Contact />} />
             <Route path="/privacy_policy" element={<PrivacyPolicy />} />
-            <Route path="/web_builder" element={<WebBuilder />} />
+            
           </Routes>
           <AIChatBot />
         
